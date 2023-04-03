707c83dd39c7a1b0fff619a475d33d023b8c0a40
Tatiana Osipova <otv120780@gmail.com>
2023-04-03 15:32:16 -0400

    feat: correct height burger menu

--- main.js
+++ main.js
@@ -7,27 +7,27 @@ menuBtn.addEventListener('click', () => {
     menu.classList.toggle('nav__list--active');
 });
 
-const tabItem = document.querySelectorAll('.tabs__btn-item');
-const tabContent = document.querySelectorAll('.tabs__content-item');
+// const tabItem = document.querySelectorAll('.tabs__btn-item');
+// const tabContent = document.querySelectorAll('.tabs__content-item');
 
-tabItem.forEach(function(element) {
-    element.addEventListener('click', open);
-});
+// tabItem.forEach(function(element) {
+//     element.addEventListener('click', open);
+// });
 
-function open(evt){
-    const tabTarget = evt.currentTarget;
-    const button = tabTarget.dataset.button;
+// function open(evt){
+//     const tabTarget = evt.currentTarget;
+//     const button = tabTarget.dataset.button;
 
-    tabItem.forEach(function(item){
-        item.classList.remove('tabs__btn-item--active');
-    });
+//     tabItem.forEach(function(item){
+//         item.classList.remove('tabs__btn-item--active');
+//     });
 
-    tabContent.forEach(function(item){
-        item.classList.remove('tabs__content-item--active');
-    });
+//     tabContent.forEach(function(item){
+//         item.classList.remove('tabs__content-item--active');
+//     });
 
-    tabTarget.classList.add('tabs__btn-item--active');
-    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
-}
+//     tabTarget.classList.add('tabs__btn-item--active');
+//     document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+// }
 
 
